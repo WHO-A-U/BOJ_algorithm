ed82942cfcbc25f07b514442351bb95b5a13fe3f
WHO-A-U <59171080+WHO-A-U@users.noreply.github.com>
2021-02-27 21:39:53 +0900

    BOJ

--- 10973.cpp
+++ 10973.cpp
@@ -5,40 +5,17 @@ using namespace std;
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    int n,prev,x;
+    int n;
     cin>>n;
-    vector<int> d(n,0);
+    vector<int> a(n,0);
     for(int i=0;i<n;i++){
-        cin>>d[i];
+        a[i]=i+1;
     }
-    int idx=-1;
-    for(int i=0;i<n-1;i++){
-        if(d[i]>d[i+1]){
-            idx=i;
+    do{
+        for(int i=0;i<a.size();i++){
+            cout<<a[i]<<" ";
         }
-    }
-    if(idx==-1){
-        cout<<"-1";
-        return 0;
-    }
-    int m=0;
-    for(int i=idx+1;i<n;i++){
-        if(d[idx]>d[i])
-            m = max(m,d[i]);
-    }
-    vector<int> a;
-    for(int i=idx;i<n;i++){
-        if(d[i]!=m)
-            a.push_back(d[i]);
-    }
-    sort(a.begin(),a.end());
-    reverse(a.begin(),a.end());
-    for(int i=0;i<idx;i++){
-        cout<<d[i]<<" ";
-    }
-    cout<<m<<" ";
-    for(int x : a){
-        cout<<x<<" ";
-    }
+        cout<<"\n";
+    }while(next_permutation(a.begin(),a.end()));
 
 }
