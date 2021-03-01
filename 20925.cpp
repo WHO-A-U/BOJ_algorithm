762af36c4d4d69a763096cc962ec0e7b60b7a895
WHO-A-U <59171080+WHO-A-U@users.noreply.github.com>
2021-03-01 17:34:28 +0900

    BOJ

--- 20925.cpp
+++ 20925.cpp
@@ -4,60 +4,60 @@
 #include<queue>
 #include<cstring>
 using namespace std;
-int d[2002][201];
+using pii = pair<int ,int>;
+int d[1002][201];  // t ,v    t시간에 v지점에서 최대 점수
 vector<int> e[201];
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     int n,t,a,b;
     cin>>n>>t;
+
+    for(int i=0;i<=t;i++){
+        for(int j=0;j<n;j++){
+            d[i][j]=-1;
+        }
+    }
+
     vector<int> entry(n,0);
     vector<int> exp(n,0);
-    vector<int> start;
+
     for(int i=0;i<n;i++){
         cin>>entry[i]>>exp[i];
         if(entry[i]==0){
-            start.push_back(i);
+            d[0][i]=0;
+
         }
     }
+
     for(int i=0;i<n;i++){
         e[i].assign(n,0);
         for(int j=0;j<n;j++){
             cin>>e[i][j];
         }
     }
-    for(int i=0;i<=t;i++){
+
+    for(int i=1;i<=t;i++){
         for(int j=0;j<n;j++){
-            d[i][j]=-1;
-        }
-    }
-    queue<pair<int,int> >q; //시간 , 지역
-    for(int i=0;i<start.size();i++){
-        q.push(make_pair(0,start[i]));
-        d[0][start[i]]=0;
-    }
-    while(!q.empty()){
-        int curTime = q.front().first;
-        int area = q.front().second;
-        int curExp = d[curTime][area];
-        q.pop();
-        if(curTime+1<=t && d[curTime+1][area]<curExp+exp[area]){
-            d[curTime+1][area]=curExp+exp[area];
-            q.push(make_pair(curTime+1,area));
-        }
-        for(int i=0;i<n;i++){
-            if(i==area)continue;
-            int cost = e[area][i];
-            if(curTime+cost<=t&&curExp>=entry[i]&&d[curTime+cost][i] < curExp){
-                d[curTime+cost][i] = curExp;
-                q.push(make_pair(curTime+cost,i));
+            for(int k=0;k<n;k++){
+                if(j==k){
+                    if(d[i-1][j]>=0){
+                        d[i][j]=max(d[i][j],d[i-1][j]+exp[j]);
+                    }
+                }else{
+                    if(i-e[j][k]>=0&&d[i-e[j][k]][k]>=entry[j]){
+                        d[i][j]=max(d[i][j],d[i-e[j][k]][k]);
+                    }
+                }
             }
         }
     }
+    
     int ans=0;
     for(int i=0;i<n;i++){
         ans = max(ans , d[t][i]);
     }
+    
     cout<<ans;
     return 0;
 }
